124a1bd6a3293172abaffc16a4156ea22a14fa4a
puzanovma <puzanovma@yandex.ru>
2023-08-09 16:37:11 +0400

    первоначальное заполнение таблицы

---
+++ dz1/populate.sql
@@ -0,0 +1,4 @@
+create table persons(id serial, first_name text, second_name text); 
+insert into persons(first_name, second_name) values('ivan', 'ivanov'); 
+insert into persons(first_name, second_name) values('petr', 'petrov'); 
+commit;
